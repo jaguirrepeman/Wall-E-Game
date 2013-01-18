4a7ebb9983e267775a2367995074f716163a737b
j.dan.por <j.dan.por@4cc90548-9c7f-b574-e9b7-a7adba6a2081>
2013-01-18 09:35:10 +0000

    fucking commit

---
+++ Pr3/src/tp/pr3/items/Garbage.java
@@ -0,0 +1,36 @@
+package tp.pr3.items;
+
+import tp.pr3.Place;
+import tp.pr3.RobotEngine;
+
+public class Garbage extends Item {
+
+	public Garbage(String id, String description, int recycledMaterial) {
+		super(id, description);
+		this.recycledMaterial = recycledMaterial;
+	}
+
+	@Override
+	public boolean canBeUsed() {
+		return this.recycledMaterial != 0;
+
+	}
+
+	@Override
+	public boolean use(RobotEngine r, Place p) {
+		if (this.canBeUsed()) {
+			r.addRecycledMaterial(recycledMaterial);
+			this.recycledMaterial = 0;
+			return true;
+		} else
+			return false;
+
+	}
+
+	public String toString() {
+		return this.getId() + ": " + super.toString()
+				+ "// recycled material = " + this.recycledMaterial;
+	}
+
+	private int recycledMaterial;
+}
