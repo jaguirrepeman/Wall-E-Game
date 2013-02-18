dd2c472552fde68091bc69d12e40ee729e9f6963
j.dan.por <j.dan.por@gmail.com>
2013-02-18 10:39:26 +0000

    Trabaja Patán

---
+++ Pr3/src/cityLoader/CityLoaderFromTxtFile.java
@@ -0,0 +1,13 @@
+package cityLoader;
+
+import tp.pr3.Place;
+
+public class CityLoaderFromTxtFile {
+	
+	public CityLoaderFromTxtFile(){
+		
+	}
+	public Place getInitialPlace(){
+		return null;
+	}
+}
--- Pr3/src/tp/pr3/cityLoader/cityLoaderExceptions/CityLoaderFromTxtFile.java
+++
@@ -1,8 +0,0 @@
-package tp.pr3.cityLoader.cityLoaderExceptions;
-
-public class CityLoaderFromTxtFile {
-	
-	public CityLoaderFromTxtFile(){
-		
-	}
-}
