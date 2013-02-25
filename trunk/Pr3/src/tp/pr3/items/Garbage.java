94abf6721d45fbbd4b98338e7f14b6dec2af38a4
j.dan.por@gmail.com <j.dan.por@gmail.com@4cc90548-9c7f-b574-e9b7-a7adba6a2081>
2013-02-25 09:58:37 +0000

    cosas del garbage

--- trunk/Pr3/src/tp/pr3/items/Garbage.java
+++ trunk/Pr3/src/tp/pr3/items/Garbage.java
@@ -8,11 +8,12 @@ public class Garbage extends Item {
 	public Garbage(String id, String description, int recycledMaterial) {
 		super(id, description);
 		this.recycledMaterial = recycledMaterial;
+		this.hasBeenUsed = false;
 	}
 
 	@Override
 	public boolean canBeUsed() {
-		return this.recycledMaterial != 0;
+		return this.hasBeenUsed;
 
 	}
 
@@ -21,6 +22,7 @@ public class Garbage extends Item {
 		if (this.canBeUsed()) {
 			r.addRecycledMaterial(recycledMaterial);
 			this.recycledMaterial = 0;
+			this.hasBeenUsed = true;
 			return true;
 		} else
 			return false;
@@ -33,4 +35,5 @@ public class Garbage extends Item {
 	}
 
 	private int recycledMaterial;
+	private boolean hasBeenUsed;
 }
